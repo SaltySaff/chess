23eb46696f85898a804b53ccdcfa0da31151424a
SaltySaff <danielalexander.ash@gmail.com>
2022-01-26 19:52:28 +0000

    Add movesets for more basic pieces

--- lib/piece.rb
+++ lib/piece.rb
@@ -4,11 +4,17 @@ class Piece
   def initialize(piece_type, color)
     @piece_type = piece_type
     @color = color
-    moveset = get_moveset(piece_type)
-    active = true
+    @moveset = get_moveset(piece_type)
+    @active = true
   end
 
   def get_moveset(piece_type)
     case piece_type
+    when 'knight'
+      @moveset = [[1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1], [-2, 1], [-1, 2]].freeze
+    when 'pawn'
+      @moveset = [1, 0].freeze
+    when 'king'
+      @moveset = [[1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1], [1, -1], [1, 0]].freeze
     end
   end
