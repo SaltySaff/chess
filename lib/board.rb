d1bbecd0aa3ac98ceadead1c7906ebaf67f541a8
SaltySaff <danielalexander.ash@gmail.com>
2022-01-27 13:38:46 +0000

    Create preliminary version of chess board

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,19 @@
 # frozen_string_literal: true
 
+# creates/modifies game board
 class Board
-  def initialize; end
+  def initialize
+    @board_cells = Array.new(8) { Array.new(8, ' ') }
+  end
+
+  def display
+    puts '  +---+---+---+---+---+---+---+---+'.light_blue
+    n = 8
+    while n >= 1
+      puts "#{n.to_s.green} #{'|'.blue} #{@board_cells[n - 1].join(' | '.light_blue)} #{'|'.light_blue}"
+      puts '  +---+---+---+---+---+---+---+---+'.light_blue
+      n -= 1
+    end
+    puts '    a   b   c   d   e   f   g   h'.green
+  end
 end
-    
--- lib/main.rb
+++ lib/main.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
+require 'colorize'
+
 require_relative '../lib/board'
 require_relative '../lib/game'
 require_relative '../lib/piece'
 
-rook = Piece.new('queen', 'black')
-p rook.moveset
+# rook = Piece.new('queen', 'black')
+# p rook.moveset
+
+board = Board.new
+board.display
